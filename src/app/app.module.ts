a715659b6bda301a5bf7df60d3e8a3cffde5cf89
swimauger <swimauger@gmail.com>
2021-04-07 10:07:02 -0500

    :sparkles: Add title screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleScreenComponent } from './components/title-screen/title-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleScreenComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/title-screen/title-screen.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'title-screen',
+  templateUrl: './title-screen.component.html',
+  styleUrls: ['./title-screen.component.scss']
+})
+export class TitleScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
