793f4aa7d73afc064a55b0d42fab7a164e36482f
swimauger <swimauger@gmail.com>
2021-04-09 16:13:51 -0500

    🐛 Fix improper routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'job/plaid', redirectTo: '/' }
+  {
+    path: 'job',
+    children: [
+      { path: 'plaid', redirectTo: '/' }
+    ]
+  }
 ];
 
 @NgModule({
